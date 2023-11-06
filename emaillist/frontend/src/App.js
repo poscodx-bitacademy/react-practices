6a8c758403b97a4818002e60e0e819eb7153a36f
poscodx-bitacademy <poscodx.bitacademy@gmail.com>
2023-11-06 16:54:31 +0900

    emailist #3

--- emaillist/frontend/src/App.js
+++ emaillist/frontend/src/App.js
@@ -1,22 +1,69 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import './assets/scss/App.scss';
 import RegisterForm from './RegisterForm';
 import SearchBar from './SearchBar';
 import Emaillist from './Emaillist';
-import data from './assets/json/data';
+// import data from './assets/json/data';
 
 function App() {
-    const [emails, setEmails] = useState(data);
+    const [emails, setEmails] = useState(null);
     const searchEmail = (keyword) => {
         const newEmails = data.filter(email => email.firstName.indexOf(keyword) !== -1 || email.lastName.indexOf(keyword) !== -1 || email.email.indexOf(keyword) !== -1);
         setEmails(newEmails);
     };
 
+    const addEmail = async (email) => {
+        fetch('/api', {
+            method: 'post',
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json'
+            },
+            body: JSON.stringify(email)
+        });
+        
+    }
+
+    const fetchList = async () => {
+        try {
+            const response = await fetch('/api', {
+                method: 'get',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Accept': 'application/json'
+                },
+                body: null
+            });
+
+            if(!response.ok) {
+                throw new Error(`${response.status} ${response.statusText}`)
+            }
+
+            const json = await response.json();
+
+            if(json.result !== 'success') {
+                throw new Error(`${json.result} ${json.message}`)
+            }
+
+            console.log(json.data);
+            setEmails(json.data);
+        } catch(err) {
+            console.error(err);
+        }
+    }
+
+    useEffect(() => {
+        fetchList();
+    }, []);
+
     return (
         <div id={'App'}>
-            <RegisterForm />
+            <RegisterForm addEmail={addEmail} />
             <SearchBar searchEmail={searchEmail}/>
-            <Emaillist emails={emails} />
+            { emails === null ?
+                null :
+                <Emaillist emails={emails} />
+            }
         </div>
     );
 }
